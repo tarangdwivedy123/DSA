9e106328569fc6bbf6d5c29178f1afd626bb43b6
Tarang Dwivedy <50951467+tarangdwivedy123@users.noreply.github.com>
2024-11-06 02:35:04 +0530

    Update 0009-palindrome-number.cpp

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) return false; // Negative numbers are not palindromes
+        if (x < 0) return false; 
         vector<int> t;
         int original = x;
         
-        // Populate the vector with digits of the number
+       
         while (x != 0) {
             t.push_back(x % 10);
             x = x / 10;
         }
         
-        // Check if the number is a palindrome
         for (int i = 0; i < t.size() / 2; ++i) {
             if (t[i] != t[t.size() - i - 1]) {
                 return false;
