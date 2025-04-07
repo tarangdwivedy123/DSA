4c7d0a0eeeaa4b678f35ff61594fd86a90cc4f10
Tarang Dwivedy <50951467+tarangdwivedy123@users.noreply.github.com>
2025-04-07 23:16:12 +0530

    Time: 0 ms (100%), Space: 14.8 MB (49.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector <pair<int,int>> nums_with_index;
 
-      
-      vector<pair<int, int>> numWithIndex;
-        for (int i = 0; i < nums.size(); i++) {
-            numWithIndex.push_back({nums[i], i});
+        for(int i=0;i<nums.size();i++){
+            nums_with_index.push_back({nums[i],i});
         }
-      sort(numWithIndex.begin(), numWithIndex.end());
 
-      int low = 0;
-      int high = nums.size()-1;
-      while(low<high){
-        int sum = numWithIndex[low].first + numWithIndex[high].first;
-        if(sum == target){
-             return {numWithIndex[low].second, numWithIndex[high].second};
-        }else if(sum > target){
-             high--;
+        sort(nums_with_index.begin(),nums_with_index.end());
+
+        int low = 0;
+        int high = nums_with_index.size()-1;
+        
+        while(low<high){
+            int sum = nums_with_index[low].first + nums_with_index[high].first;
+            
+            if(sum == target){
+                return{nums_with_index[low].second , nums_with_index[high].second};
+            }
+            if(sum > target){
+                high--;
+            }
+            if(sum < target){
+                low++;
+            }
         }
-        else if(sum < target){
-            low++;
-        }
-      }
-
-      return{-1,-1};
-
+        return {-1,-1};
     }
 };
