b5c5d25d4acb123fb1c014e73420885d2a8551ef
Tarang Dwivedy <50951467+tarangdwivedy123@users.noreply.github.com>
2025-04-07 22:50:01 +0530

    Time: 1 ms (81.95%), Space: 14.7 MB (55.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hash_map;
 
-        for(int i=0;i<nums.size();++i){
-            int complement = target - nums[i];
-           if(hash_map.find(complement) != hash_map.end()){
-                return{hash_map[complement], i};
-            }
-            hash_map[nums[i]] = i;
-            }
-            return{};
-    } 
+      
+      vector<pair<int, int>> numWithIndex;
+        for (int i = 0; i < nums.size(); i++) {
+            numWithIndex.push_back({nums[i], i});
+        }
+      sort(numWithIndex.begin(), numWithIndex.end());
+
+      int low = 0;
+      int high = nums.size()-1;
+      while(low<high){
+        int sum = numWithIndex[low].first + numWithIndex[high].first;
+        if(sum == target){
+             return {numWithIndex[low].second, numWithIndex[high].second};
+        }else if(sum > target){
+             high--;
+        }
+        else if(sum < target){
+            low++;
+        }
+      }
+
+      return{-1,-1};
+
+    }
 };
